f732b2d710be76181040f1113eb6667373314c5b
Duke1965 <leonardwdonaldson@gmail.com>
2025-07-08 14:27:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { SimpleMapTest } from "./components/simple-map-test"
 import { useLocationServices } from "./hooks/useLocationServices"
 import { reverseGeocode } from "./utils/geocoding"
 import { MarkerSelector } from "./components/marker-icons"
