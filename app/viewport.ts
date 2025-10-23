6a147ba4bbacfec28a7a3fdf236efaf2ac758a73
Duke1965 <leonardwdonaldson@gmail.com>
2025-10-23 13:38:00 +0200

    Update viewport.ts

--- app/viewport.ts
+++ app/viewport.ts
@@ -3,6 +3,6 @@ export const viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  viewportFit: 'cover',
+  // viewportFit: 'cover', // Removed to eliminate splash screen
   // themeColor: '#1e3a8a', // Removed to eliminate splash screen
 };
