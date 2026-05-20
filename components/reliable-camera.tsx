fb39289b096044c2932ade8f8b300bc5407a97e3
Duke1965 <leonardwdonaldson@gmail.com>
2026-05-20 08:32:26 +0200

    Update reliable-camera.tsx

--- components/reliable-camera.tsx
+++ components/reliable-camera.tsx
@@ -324,6 +324,8 @@ export function ReliableCamera({ mode, onCapture, onClose }: ReliableCameraProps
             width: "100%",
             height: "100%",
             objectFit: "cover",
+            opacity: cameraReady ? 1 : 0,
+            transition: "opacity 120ms ease",
           }}
           onTouchStart={handleFocus}
           onClick={handleFocus}
