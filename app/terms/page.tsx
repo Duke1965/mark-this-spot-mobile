aae727395ddb1abcf7601704082ae3f2b629a9ce
Duke1965 <leonardwdonaldson@gmail.com>
2026-05-25 09:24:37 +0200

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -71,10 +71,9 @@ export default function TermsPage() {
             These terms may be updated from time to time.
           </div>
           <div>
-            Contact:{" "}
-            <a href="mailto:support@app-titude.co" style={{ color: "white", fontWeight: 900, textDecoration: "underline" }}>
-              support@app-titude.co
-            </a>
+            Contact:
+            <br />
+            support@app-titude.com
           </div>
         </div>
       </div>
